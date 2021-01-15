28eb11749273aefed6042e4a2d089e6cc31ae0ad
roberto <rob.sosa13@gmail.com>
2021-01-15 18:05:16 -0400

    15012021

--- src/index.js
+++ src/index.js
@@ -1,9 +1,31 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import Test from '../src/components/planilla/Test'
+
+
+// function formatName(user) {
+//   return user.firstName + ' ' + user.lastName;
+// }
+
+// const user = {
+//   firstName: 'Harper',
+//   lastName: ´+
+// };
+
+// const element = (
+//   <h1>
+//     Hello, {formatName(user)}!
+//   </h1>
+// );
+
+// ReactDOM.render(
+//   element,
+//   document.getElementById('root')
+// );
 ReactDOM.render(
     <div className="App">
-       <App />
+       <Test />
     </div>
 ,
   document.getElementById('root')
